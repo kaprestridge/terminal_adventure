e2b765337959c26b38a8a8dd141da966f4a0a187
kaprestridge <kaprestridge@gmail.com>
2020-12-15 20:15:59 +1100

    fixed character creator, is working now

--- character_creator.rb
+++ character_creator.rb
@@ -1,3 +1,6 @@
+require 'tty-prompt'
+prompt = TTY::Prompt.new
+
 class Hero
     attr_accessor :str, :dex, :con, :int, :wis, :cha
     def initialize(name)
@@ -36,19 +39,17 @@ hero = Hero.new(hero_name)
 
 
 class Hero_Questions (Hero)
+    attr_reader :answer_options
     def initialize (question_text, answer_options)
         @question_text = question_text
-        @answer_options = answer_options[0..]
+        @answer_options = answer_options
     end
     def to_s
         @question_text
     end
 end
 
-question1 = Hero_Questions.new(
-    "What is your favorite colour?",
-    answer_options1 = {"blue" => hero.change_str(1), "green" => hero.change_con(1)}
-)
+{"blue" => proc { hero.change_str(1) } }
 
 
 
@@ -60,11 +61,31 @@ puts hero.int
 puts hero.wis
 puts hero.cha
 
-puts question1
+question_list = [
+question1 = Hero_Questions.new(
+    "What is your favorite colour?",
+    answer_options = {"blue" => proc { hero.change_str(1)}, "pink" => proc { hero.change_dex(1)} }
+),
+question2 = Hero_Questions.new(
+    "What is your favorite colour?2",
+    answer_options = {"blue" => proc { hero.change_str(1)}, "pink" => proc { hero.change_dex(1)} }
+),
+question3 = Hero_Questions.new(
+    "What is your favorite colour?3",
+    answer_options = {"blue" => proc { hero.change_str(1)}, "pink" => proc { hero.change_dex(1)} }
+),
+last_question = Hero_Questions.new(
+    "What is your favorite colour?4",
+    answer_options = {"blue" => proc { hero.change_str(1)}, "pink" => proc { hero.change_dex(1)} }
+)
+]
 
 
-
-
+question_list.each { |q, a| 
+puts q
+menu_options = answer_options.map{|option, index| {name: option, value: index} }
+hero_answer = prompt.select('Your answer:', menu_options)
+}
 
 
 puts hero.str
